85b18ddd886c81d6e7505b76ea0b9b3722eaa376
PhamNgocManh <manh.pn2002@gmail.com>
2024-05-27 15:30:02 +0700

    Complete FlightDocsSystem

--- ReportingService/Models/UserActivityReport.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ReportingService.Models
-{
-    public class UserActivityReport
-    {
-        public int UserId { get; set; }
-        public string Action { get; set; }
-        public DateTime ActionTime { get; set; }
-    }
-}
